81ee049b0a913abb314037c4164d0e62b0d61f7a
Matthew Songer <matt91893@gmail.com>
2022-06-28 21:56:19 -0500

    add mutations to typeDef and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,27 @@
-const { User, Thought } = require('../models')
-
+const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 const resolvers = {
     Query: {
-        thoughts: async () => {
-            return Thought.find().sort({ createdAt: - 1 });
+      // logic here remains the same
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+
+        return user;
+      },
+      login: async (parent, {email, password}) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
         }
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+        return user;
+      }
     }
-};
+  };
 
 module.exports = resolvers;
